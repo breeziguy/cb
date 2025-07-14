50a69f3c89a802bda5c60f7624c6d6e77903a490
Breeziguy <mrolabola@gmail.com>
2025-07-14 00:33:29 -0700

    Fix customers page build error - add missing props to Overview component
    - Import required mock data (overview, customersOverviewChartData)
    - Transform overview data to match expected interface
    - Pass data, monthlyStats, and topCreators props to Overview component
    - Resolves 'Cannot read properties of undefined (reading map)' error

--- templates/Customers/OverviewPage/index.tsx
+++ templates/Customers/OverviewPage/index.tsx
@@ -8,14 +8,28 @@ import ActiveTimes from "./ActiveTimes";
 // import RefundRequests from "./RefundRequests";
 import Countries from "./Countries";
 import Messages from "./Messages";
-import { devicesChartData, devicesGenderData } from "@/mocks/charts";
+import { devicesChartData, devicesGenderData, customersOverviewChartData } from "@/mocks/charts";
+import { overview } from "@/mocks/customers";
 
 const OverviewPage = () => {
+    // Transform overview data to match expected format
+    const transformedOverviewData = overview.map(item => ({
+        id: item.id.toString(),
+        title: item.title,
+        value: parseInt(item.counter.replace(/k/g, '000').replace(/,/g, '')),
+        icon: item.icon,
+        color: "#10b981" // default green color
+    }));
+
     return (
         <div className="center-with-sidebar pb-5">
             <div className="flex max-lg:flex-col">
                 <div className="col-left">
-                    <Overview />
+                    <Overview 
+                        data={transformedOverviewData}
+                        monthlyStats={customersOverviewChartData}
+                        topCreators={[]}
+                    />
                     <TrafficСhannel />
                     <ActiveTimes />
                 </div>
